d80add35972b4f6ecba4e9b1e317aa8ad9394542
ghosfygigo5678 <41460251+ghosfygigo5678@users.noreply.github.com>
2018-07-30 12:43:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -764,7 +764,7 @@ client.on('message', message => {
 var embed = new Discord.RichEmbed()
 .setColor('RANDOM')
  .setThumbnail(message.author.avatarURL) 
-.addField('sliver BOT' ,
+.addField('Dollar BOT' ,
 `${Za7f[Math.floor(Math.random() * Za7f.length)]}`)
 message.channel.sendEmbed(embed);
 console.log('[38ab] Send By: ' + message.author.username)
