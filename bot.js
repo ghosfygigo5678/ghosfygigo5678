6577c1c8feebf7e1ded614891d045d8cb7a35c78
ghosfygigo5678 <41460251+ghosfygigo5678@users.noreply.github.com>
2018-07-30 09:37:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -157,7 +157,7 @@ client.on("message", message => {
         color: 0x06DF00,
         description: "تم مسح الرسائل بنجاح",
         footer: {
-          text: "yazan"
+          text: "Dollar"
         }
       }}).then(msg => {msg.delete(3000)});
                           }
@@ -2345,7 +2345,7 @@ client.on('message', message => {
     .setColor("#707070")
     .addField(': دخولك لديسكورد قبل', `${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm:ss')} **\n** \`${moment(heg.createdTimestamp).fromNow()}\`` ,true) 
     .addField(': انضمامك لسيرفر قبل', `${moment(h.joinedAt).format('YYYY/M/D HH:mm:ss')} \n \`${moment(h.joinedAt).fromNow()}\``, true)               
-    .setFooter(`sliver Bot`, 'https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif')                                 
+    .setFooter(`Dollar Bot`, 'https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif')                                 
     .setThumbnail(heg.avatarURL);
     message.channel.send(id)
 }       });
@@ -2495,7 +2495,7 @@ const channel = sWlc[message.guild.id].channel
 		     	var prefix ="%";
                 if(message.content === prefix + "inv") {
                     let embed = new Discord.RichEmbed ()
-                    embed.setTitle("arrow_right Invite Dollar Bot!")
+                    embed.setTitle("arrow_right Invite Dollar Bot%")
                     .setURL("https://discordapp.com/api/oauth2/authorize?client_id=473363587986096151&permissions=485505073&scope=bot");
                    message.channel.sendEmbed(embed);
                   }
