0c1d1d893b6934fde6294f4c2c2862e6e2f992ce
ghosfygigo5678 <41460251+ghosfygigo5678@users.noreply.github.com>
2018-07-30 04:16:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '%'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
+client.user.setGame(`%help.%inv`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
