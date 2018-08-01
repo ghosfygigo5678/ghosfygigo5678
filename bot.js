dd49908ef833cb404159cd01cea7ad8facf013c9
ghosfygigo5678 <41460251+ghosfygigo5678@users.noreply.github.com>
2018-08-01 11:33:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,4 @@
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+console.log(`Logged in as ${client.user.tag}!`);
 client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
